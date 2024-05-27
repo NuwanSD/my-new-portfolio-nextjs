87119f05928a2176c8ce01fce7733979372257d7
NuwanSD <sagaradnuwan@gmail.com>
2024-05-27 23:47:26 +0530

    added more features

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,27 @@
 import React from "react";
-import { FaHome } from "react-icons/fa";
 
 import Hero from "@/components/shared/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import Grid from "@/components/shared/Grid";
 import RecentProject from "@/components/shared/RecentProject";
 import { navItems } from "@/data";
+import Clients from "@/components/shared/Clients";
+import Experience from "@/components/shared/Experience";
+import Process from "@/components/shared/Process";
+import Footer from "@/components/shared/Footer";
 
 const page = () => {
   return (
-    <main className=" relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className=" relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProject />
+        <Clients />
+        <Experience />
+        <Process />
+        <Footer />
       </div>
     </main>
   );
